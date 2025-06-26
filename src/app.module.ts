0d3cc8393de51db997b7ebc646f355717cf6ca3b
mjmmattoni98 <javier.magni@gmail.com>
2025-06-26 23:00:50 +0200

    Remove unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CandidatesModule } from './candidates/candidates.module';
 
 @Module({
   imports: [CandidatesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
